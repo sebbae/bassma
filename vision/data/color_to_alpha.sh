f52fa98861aa6ee882c281615031e47b6e2c3102
Sebastian Baechle <seb.baechle@gmail.com>
2014-01-21 22:03:52 +0100

    move data and scripts

--- vision/data/color_to_alpha.sh
+++
@@ -1,1 +0,0 @@
-for FILE in *.png; do convert P1090270_free.png -fuzz 23% -transparent black P1090270_free_free.png; done
--- vision/data/crop_by_color.sh
+++
@@ -1,1 +0,0 @@
-for FILE in *.JPG; do convert P1090270.JPG -fuzz 25% -trim +repage P1090270.png; done
