c0b8cb9fdf6d542cfa05f4e357479e7b7f0e5637
Sebastian Baechle <seb.baechle@gmail.com>
2014-04-01 22:01:46 +0200

    qt example is working

--- simulation/src/qt/qtexample.cpp
+++ simulation/src/qt/qtexample.cpp
@@ -1,19 +1,28 @@
-/*
- * qtexample.cpp
- *
- *  Created on: 30.03.2014
- *      Author: sebastian
- */
-
 #include <QApplication>
-#include "cqtopencvviewergl.h"
+#include <QDebug>
 
-int main(int argv, char **args) {
-	QApplication app(argv, args);
+#include "qtglwebcamdemo.h"
 
-	CQtOpenCVViewerGl viewer;
-	viewer.show();
+int main(int argc, char *argv[])
+{
+    int res=-1;
 
-	return app.exec();
+    try
+    {
+        QApplication a(argc, argv);
+        QtGLWebcamDemo w;
+        w.show();
+
+        res = a.exec();
+    }
+//    catch(QException &e)
+//    {
+//        qCritical() << QString("Exception: %1").arg( e.what() );
+//    }
+    catch(...)
+    {
+        qCritical() << QString("Unhandled Exception");
+    }
+
+    return res;
 }
-
